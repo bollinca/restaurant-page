a459119e5f931b9ad98bda1d2d587835229d84b1
bollinca <51376777+bollinca@users.noreply.github.com>
2019-09-02 15:55:04 -0400

    Add form, adjust style

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -5,8 +5,17 @@ const createContact = () => {
     const contactHeader = document.createElement('h2');
     const bulkParagraph = document.createElement('p');
     const helpParagraph = document.createElement('p');
+
     const formHeader = document.createElement('h3');
     const contactForm = document.createElement('form');
+    const formName = document.createElement('input');
+    formName.setAttribute('type', 'text');
+    formName.setAttribute('placeholder', 'Name');
+    const formQuestion = document.createElement('textarea');
+    formQuestion.setAttribute('rows', '8');
+    formQuestion.setAttribute('placeholder', 'Your Question Goes Here.');
+    const formButton = document.createElement('button');
+    formButton.textContent = 'Submit Question';
 
     contactHeader.textContent = 'CONTACT INFORMATION';
     bulkParagraph.textContent = 'For bulk order inqueries, please call (555) 867-5309, or email us at fakeemail@fake.com';
@@ -17,6 +26,11 @@ const createContact = () => {
     contactDiv.appendChild(bulkParagraph);
     contactDiv.appendChild(helpParagraph);
     contactDiv.appendChild(formHeader);
+
+    contactForm.appendChild(formName);
+    contactForm.appendChild(formQuestion);
+    contactForm.appendChild(formButton);
+
     contactDiv.appendChild(contactForm);
 
     container.appendChild(contactDiv);
